6fc0f30b7de6d4461fe08a75c73dce8192e0a317
Andrew <andrew@thelucks.org>
2017-04-07 11:57:39 -0400

    Adding new app

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-var cool = require('cool-ascii-faces');
 var express = require('express');
+var fs = require('fs');
 var app = express();
+var http = require('http');
+
+function readJsonFileSync(filepath, encoding){
+
+    if (typeof (encoding) == 'undefined'){
+        encoding = 'utf8';
+    }
+    var file = fs.readFileSync(filepath, encoding);
+    return JSON.parse(file);
+}
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -11,11 +21,49 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+  jsonData = readJsonFileSync('./json/teams.json');
+  purse = readJsonFileSync('./json/purse.json');
+var body = "";
+  http.get({ host: 'www.pgatour.com', path: '/data/r/014/2017/leaderboard-v2.json' }, function(res) { 
+  res.on('data', function(chunk) {
+    body += chunk;
+  });
+  res.on('end', function() {
+  leaderboard = JSON.parse(body);
+  var players = {};
+  for (var i = 0, len = leaderboard["leaderboard"]["players"].length; i < len; i++) {
+        var jsonPlayer = leaderboard["leaderboard"]["players"][i];
+	var player = {};
+	player["name"] = jsonPlayer["player_bio"]["first_name"] + " " + jsonPlayer["player_bio"]["last_name"];
+        player["position"] = jsonPlayer["current_position"];
+        player["thru"] = jsonPlayer["thru"];
+	players[jsonPlayer["player_id"]] = player;
+   }
+	
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
+  for(var i = 0, len = jsonData['teams'].length; i < len; i++) {
+	var winnings = 0;
+	response.write(jsonData['teams'][i]["Name"] + "\n");
+	response.write("----------------------------\n");
+        for (var j = 0, len2 = jsonData['teams'][i]['Players'].length; j < len2; j++) {
+        var player = players[jsonData['teams'][i]['Players'][j]];
+        var thru = player["thru"] === null ? "0" : player["thru"];
+	response.write(player["name"] + " - " + player["position"] + " thru " + thru + "\n");
+
+	var position = player["position"];
+	
+	var rank = parseInt(position.replace('T', ''));
+	if (rank <= 50) {
+		winnings += purse["purse"][rank - 1];
+	}
+	}
+	response.write("Current Winnings: " + winnings);
+	response.write("\n");
+	response.write("\n");
+	}
+response.send();
+  });
+  });
 });
 
 app.listen(app.get('port'), function() {
