0e9041b4590bf1508e35e8fa7284917b890d0e07
Andrew <andrew@thelucks.org>
2017-04-07 12:16:15 -0400

    more

--- index.js
+++ index.js
@@ -37,6 +37,8 @@ var body = "";
 	player["name"] = jsonPlayer["player_bio"]["first_name"] + " " + jsonPlayer["player_bio"]["last_name"];
         player["position"] = jsonPlayer["current_position"];
         player["thru"] = jsonPlayer["thru"];
+        player["total"] = jsonPlayer["total"];
+        player["today"] = jsonPlayer["today"] === null ? "0" : jsonPlayer["today"];
 	players[jsonPlayer["player_id"]] = player;
    }
 	
@@ -44,11 +46,16 @@ var body = "";
   for(var i = 0, len = jsonData['teams'].length; i < len; i++) {
 	var winnings = 0;
 	response.write(jsonData['teams'][i]["Name"] + "\n");
-	response.write("----------------------------\n");
+	response.write("-----------------------------------\n");
         for (var j = 0, len2 = jsonData['teams'][i]['Players'].length; j < len2; j++) {
         var player = players[jsonData['teams'][i]['Players'][j]];
         var thru = player["thru"] === null ? "0" : player["thru"];
-	response.write(player["name"] + " - " + player["position"] + " thru " + thru + "\n");
+	var buffer = 20 - player["name"].length;
+	response.write(player["name"]);
+	for (var x = 0; x < buffer; x++) {
+		response.write(" ");
+	}
+  	response.write(player["total"] + " (today: " + player["today"] + ")\t" + player["position"] + " thru " + thru + "\n");
 
 	var position = player["position"];
 	
