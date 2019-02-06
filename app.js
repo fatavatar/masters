df5eabfb6eee588df43a0ce6976f328f6124b299
Andrew Luck <fatavatar@hotmail.com>
2019-02-05 21:16:21 -0500

    Adding in tab control

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ const selector = '.mdc-button, .mdc-icon-button, .mdc-card__primary-action';
 const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
   return new MDCRipple(el);
 });
+
+import {MDCTabBar} from '@material/tab-bar';
+
+const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
